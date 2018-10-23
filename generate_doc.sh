3f4c521aedc2c48b8103dc2d136503436e49a901
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2018-10-23 11:17:39 +0900

    Update generate_doc.sh
    edit moving path

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/CUBRID/cubrid.git
-mv ./cubrid/* ./
+git clone https://github.com/CUBRID/cubrid.git cubrid
+rsync -a $TRAVIS_BUILD_DIR/cubrid/ $TRAVIS_BUILD_DIR/
+rm -rf $TRAVIS_BUILD_DIR/cubrid
 doxygen Doxyfile

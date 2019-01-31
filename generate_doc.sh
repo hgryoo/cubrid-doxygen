36df9c975df083234e8dcfecdea890c67f41dad1
Hyung-Gyu Ryoo <hgryoo@cubrid.com>
2019-01-31 17:23:29 +0900

    set call graph and caller graph YES and disable latex

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/CUBRID/cubrid.git cubrid
+git clone --recursive-submodules https://github.com/CUBRID/cubrid.git cubrid
 rsync -a $TRAVIS_BUILD_DIR/cubrid/ $TRAVIS_BUILD_DIR/
 rm -rf $TRAVIS_BUILD_DIR/cubrid
 doxygen Doxyfile
